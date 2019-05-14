ffa27f8ce0dacb5c783f0faf2f900acd69e5e0e9
hieuht <thienhieuho@Hotmail.com>
2019-05-14 08:30:50 +0700

    Finished test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# test1
+# How to build project
+
+This project is base on Java language, using maven. So, to build this project, go to the source directory and run the command:
+
+```sh
+$ mvn clean install
+```
--- src/main/java/test/util/BillUtils.java
+++ src/main/java/test/util/BillUtils.java
@@ -8,17 +8,17 @@ public class BillUtils {
 
     public static double calculateNetAmount(final Bill bill) {
         double percentDiscount = 0;
-        if(bill.getCustomer().isEmployee()) {
+        if (bill.getCustomer().isEmployee()) {
             percentDiscount = 30;
-        } else if(bill.getCustomer().isAffiliate()) {
+        } else if (bill.getCustomer().isAffiliate()) {
             percentDiscount = 10;
-        } else if(DateTimeUtils.yearDiffFromNow(bill.getCustomer().getRegisteredDate()) > 2) {
+        } else if (DateTimeUtils.yearDiffFromNow(bill.getCustomer().getRegisteredDate()) > 2) {
             percentDiscount = 5;
         }
 
         final double totalBillAmountWithoutDiscount = bill.getProducts().stream().mapToDouble(Product::getPrice).sum();
         final double percentDisountAmount = calculatePercentDiscountAmount(bill, percentDiscount);
-        final double amountBaseDiscount = (totalBillAmountWithoutDiscount - percentDisountAmount) / 100 * 5;
+        final double amountBaseDiscount = (int) (totalBillAmountWithoutDiscount - percentDisountAmount) / 100 * 5;
 
         System.out.println("totalBillAmountWithoutDiscount: " + totalBillAmountWithoutDiscount);
         System.out.println("percentDisountAmount: " + percentDisountAmount);
@@ -28,14 +28,19 @@ public class BillUtils {
     }
 
     private static double calculatePercentDiscountAmount(final Bill bill, final double percentDiscount) {
-        return bill.getProducts().stream().filter(o -> isGroceries(o)).mapToDouble(o -> {
+        return bill.getProducts().stream().filter(o -> !isGroceries(o)).mapToDouble(o -> {
             return o.getPrice() * percentDiscount / 100;
         }).sum();
     }
 
     private static boolean isGroceries(final Product product) {
         final Category groceryCategory = new Category(2, "groceries");
-        return product.getCategories().contains(groceryCategory);
+//        final boolean b = product.getCategories().contains(groceryCategory);
+//        System.out.println(product.getId() + ": " + b);
+        final Category find = product.getCategories().stream()
+                .filter(o -> o.getTitle().equals(groceryCategory.getTitle())).findAny().orElse(null);
+        final boolean b = (find != null);
+        return b;
     }
 
 }
